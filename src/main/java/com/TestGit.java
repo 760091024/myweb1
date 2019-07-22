77f6371591566a3a5edfc0bff47678b604a5b3ed
760091024 <760091024@qq.com>
2019-07-22 14:59:11 +0800

    test 1.1

---
+++ src/main/java/com/TestGit.java
@@ -0,0 +1,10 @@
+package com;
+
+public class TestGit {
+
+    public static  void  main(String[] args){
+        System.out.println("----git test");
+        System.out.println("----hello world");
+    }
+
+}
